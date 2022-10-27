2d10b51af48c26f3c4f74cc6df2997455578d245
ilia <115416335+Iliathebest@users.noreply.github.com>
2022-10-27 21:48:50 +0300

    21 end

---
+++ 19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//  Console.WriteLine("Введите пятизначное число");
+
+Console.WriteLine("Введите пятизначное число");
+
+string n= Console.ReadLine(); 
+
+ void CheckingNumber(string n)
+ {
+
+  if( n[0]==n[4]&&n[1]==n[3])
+   {
+    Console.WriteLine("Является полиндромом");
+    
+   }
+ else
+   {
+      Console.WriteLine("Не является пoлиндромом");
+   }
+
+ }
---
+++ 21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+Console.WriteLine("Введите значение X1");
+int X1= int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите значение Y1");
+int Y1= int.Parse (Console.ReadLine());
+ 
+ Console.WriteLine("Введите значение Z1");
+int Z1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение X2");
+int X2= int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите значение Y2");
+int Y2= int.Parse (Console.ReadLine());
+ 
+ Console.WriteLine ("Введите значение Z2");
+int Z2= int.Parse(Console.ReadLine());
+
+double d= Math.Sqrt(Math.Pow(X2-X1,2)+Math.Pow(Y2-Y1,2)+Math.Pow(Z2-Z1,2));
+
+Console.WriteLine("расстояние между двух точек"+d);
